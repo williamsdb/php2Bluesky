7090ace47a9d84294da03a1acb1ebffcb2d6941d
Neil Thompson <github@neilthompson.co.uk>
2023-11-09 21:53:09 +0000

    Commit tested code

--- functions.php
+++ functions.php
@@ -1,195 +1,197 @@
 <?php
 
-use cjrasmussen\BlueskyApi\BlueskyApi;
+    use cjrasmussen\BlueskyApi\BlueskyApi;
 
     function bluesky_connect($handle, $password)
     {
 
-    $connection = new BlueskyApi($handle, $password);
-    return $connection;
+        $connection = new BlueskyApi($handle, $password);
+        return $connection;
 
     }
 
-    function upload_media_to_bluesky($connection, $s3name)
+    function upload_media_to_bluesky($connection, $filename)
 	{
 
-	// have we been passed a file?
-	if (empty($s3name)) return;
-    
-    $body = file_get_contents($s3name);
-    if(filter_var($s3name, FILTER_VALIDATE_URL)){
+        // have we been passed a file?
+        if (empty($filename)) return;
+        
+        $body = file_get_contents($filename);
+        if(filter_var($filename, FILTER_VALIDATE_URL)){
 
-        $headers = get_headers($s3name, 1); 
+            $headers = get_headers($filename, 1); 
 
-        if (isset($headers['Content-Type'])) {
-            $mime = $headers['Content-Type'];
-        } else {
-            $mime ='image/jpeg';
+            if (isset($headers['Content-Type'])) {
+                $mime = $headers['Content-Type'];
+            } else {
+                // shouldn't have got here so issue that needs to be handled!
+            }
+        }else{
+        $mime = mime_content_type($filename);
         }
-     }else{
-      $mime = mime_content_type($s3name);
-     }
 
-    $response = $connection->request('POST', 'com.atproto.repo.uploadBlob', [], $body, $mime);
+        $response = $connection->request('POST', 'com.atproto.repo.uploadBlob', [], $body, $mime);
 
-    $image = $response->blob;
+        $image = $response->blob;
 
-    return $image;
+        return $image;
 
-  }
+    }
 
 	function post_to_bluesky($connection, $text, $media='', $link='')
 	{
 
-    // parse for URLS
-    $urls = mark_urls($text);
-    $links = array();
-    if (!empty($urls)){
-      foreach ($urls as $url) {
-        $a = [
-          "index" => [
-            "byteStart" => $url['start'],
-            "byteEnd" => $url['end'],
-          ],
-          "features" => [
-            [
-                '$type' => "app.bsky.richtext.facet#link",
-                'uri' => $url['url'], 
+        // parse for URLS
+        $urls = mark_urls($text);
+        $links = array();
+        if (!empty($urls)){
+        foreach ($urls as $url) {
+            $a = [
+            "index" => [
+                "byteStart" => $url['start'],
+                "byteEnd" => $url['end'],
             ],
-          ],
+            "features" => [
+                [
+                    '$type' => "app.bsky.richtext.facet#link",
+                    'uri' => $url['url'], 
+                ],
+            ],
+            ];
+
+            $links[] = $a;
+        }
+        $links = [
+            'facets' =>
+            $links,
+        ];
+        }
+
+        // add any media
+        $embed = '';
+        if (!empty($media)){
+        $embed = [
+            'embed' => [
+            '$type' => 'app.bsky.embed.images',
+            'images' => [
+                [
+                'alt' => '',
+                'image' => $media,
+                ],
+            ],
+            ],
+        ];
+        }
+
+        // add any link
+        if (!empty($link)){
+        $embed = fetch_link_card($connection, $link);
+        }
+
+        // build the final arguments
+        $args = [
+        'collection' => 'app.bsky.feed.post',
+        'repo' => $connection->getAccountDid(),
+        'record' => [
+            'text' => $text,
+            'langs' => ['en'],
+            'createdAt' => date('c'),
+            '$type' => 'app.bsky.feed.post',
+        ],
         ];
 
-        $links[] = $a;
-      }
-      $links = [
-        'facets' =>
-          $links,
-      ];
-    }
+        if (!empty($embed)) $args['record'] = array_merge($args['record'], $embed);
+        if (!empty($links)) $args['record'] = array_merge($args['record'], $links);
 
-    // add any media
-    $embed = '';
-    if (!empty($media)){
-      $embed = [
-        'embed' => [
-          '$type' => 'app.bsky.embed.images',
-          'images' => [
-            [
-              'alt' => '',
-              'image' => $media,
-            ],
-          ],
-        ],
-      ];
-    }
-
-    // add any link
-    if (!empty($link)){
-      $embed = fetch_link_card($connection, $link);
-    }
-
-    // build the final arguments
-    $args = [
-      'collection' => 'app.bsky.feed.post',
-      'repo' => $connection->getAccountDid(),
-      'record' => [
-        'text' => $text,
-        'langs' => ['en'],
-        'createdAt' => date('c'),
-        '$type' => 'app.bsky.feed.post',
-      ],
-    ];
-
-    if (!empty($embed)) $args['record'] = array_merge($args['record'], $embed);
-    if (!empty($links)) $args['record'] = array_merge($args['record'], $links);
-
-    // send to bluesky
-    return $connection->request('POST', 'com.atproto.repo.createRecord', $args);
+        // send to bluesky
+        return $connection->request('POST', 'com.atproto.repo.createRecord', $args);
  
-  }
+    }
 
     function mark_urls($text) {
 
-    $regex = '/(https?:\/\/[^\s]+)/';
-    preg_match_all($regex, $text, $matches, PREG_OFFSET_CAPTURE);
+        $regex = '/(https?:\/\/[^\s]+)/';
+        preg_match_all($regex, $text, $matches, PREG_OFFSET_CAPTURE);
 
-    $urlData = array();
+        $urlData = array();
 
-    foreach ($matches[0] as $match) {
-        $url = $match[0];
-        $start = $match[1];
-        $end = $start + strlen($url);
+        foreach ($matches[0] as $match) {
+            $url = $match[0];
+            $start = $match[1];
+            $end = $start + strlen($url);
 
-        $urlData[] = array(
-            'start' => $start,
-            'end' => $end,
-            'url' => $url,
-        );
-    }
-
-    return $urlData;
-  }
-
-function fetch_link_card($connection, $url) {
-
-    // The required fields for every embed card
-    $card = [
-        "uri" => $url,
-        "title" => "",
-        "description" => "",
-    ];
-
-    // Create a new DOMDocument
-    $doc = new DOMDocument();
-
-    // Suppress errors for invalid HTML, if needed
-    libxml_use_internal_errors(true);
-
-    // Load the HTML from the URL
-    $doc->loadHTMLFile($url);
-
-    // Restore error handling
-    libxml_use_internal_errors(false);
-
-    // Create a new DOMXPath object for querying the document
-    $xpath = new DOMXPath($doc);
-
-    // Query for "og:title" and "og:description" meta tags
-    $title_tag = $xpath->query('//meta[@property="og:title"]/@content');
-    if ($title_tag->length > 0) {
-        $card["title"] = $title_tag[0]->nodeValue;
-    }
-
-    $description_tag = $xpath->query('//meta[@property="og:description"]/@content');
-    if ($description_tag->length > 0) {
-        $card["description"] = $description_tag[0]->nodeValue;
-    }
-
-    // If there is an "og:image" meta tag, fetch and upload that image
-    $image_tag = $xpath->query('//meta[@property="og:image"]/@content');
-    if ($image_tag->length > 0) {
-        $img_url = $image_tag[0]->nodeValue;
-        // Naively turn a "relative" URL (just a path) into a full URL, if needed
-        if (!parse_url($img_url, PHP_URL_SCHEME)) {
-            $img_url = $url . $img_url;
+            $urlData[] = array(
+                'start' => $start,
+                'end' => $end,
+                'url' => $url,
+            );
         }
-        
-        $image = upload_media_to_bluesky($connection, $img_url, $s3=FALSE);
+
+        return $urlData;
     }
 
-    $embed = '';
-    $embed = [
-      'embed' => [
-        '$type' => 'app.bsky.embed.external',
-        'external' => [
-            'uri' => $card['uri'],
-            'title' => $card['title'],
-            'description' => $card['description'],
-            'thumb' => $image,
-        ],
-      ],
-    ];
+    function fetch_link_card($connection, $url) {
 
-    return $embed;
+        // The required fields for every embed card
+        $card = [
+            "uri" => $url,
+            "title" => "",
+            "description" => "",
+        ];
+
+        // Create a new DOMDocument
+        $doc = new DOMDocument();
+
+        // Suppress errors for invalid HTML, if needed
+        libxml_use_internal_errors(true);
+
+        // Load the HTML from the URL
+        $doc->loadHTMLFile($url);
+
+        // Restore error handling
+        libxml_use_internal_errors(false);
+
+        // Create a new DOMXPath object for querying the document
+        $xpath = new DOMXPath($doc);
+
+        // Query for "og:title" and "og:description" meta tags
+        $title_tag = $xpath->query('//meta[@property="og:title"]/@content');
+        if ($title_tag->length > 0) {
+            $card["title"] = $title_tag[0]->nodeValue;
+        }
+
+        $description_tag = $xpath->query('//meta[@property="og:description"]/@content');
+        if ($description_tag->length > 0) {
+            $card["description"] = $description_tag[0]->nodeValue;
+        }
+
+        // If there is an "og:image" meta tag, fetch and upload that image
+        $image_tag = $xpath->query('//meta[@property="og:image"]/@content');
+        if ($image_tag->length > 0) {
+            $img_url = $image_tag[0]->nodeValue;
+            // Naively turn a "relative" URL (just a path) into a full URL, if needed
+            if (!parse_url($img_url, PHP_URL_SCHEME)) {
+                $img_url = $url . $img_url;
+            }
+            
+            $image = upload_media_to_bluesky($connection, $img_url, $s3=FALSE);
+        }
+
+        $embed = '';
+        $embed = [
+        'embed' => [
+            '$type' => 'app.bsky.embed.external',
+            'external' => [
+                'uri' => $card['uri'],
+                'title' => $card['title'],
+                'description' => $card['description'],
+                'thumb' => $image,
+            ],
+        ],
+        ];
+
+        return $embed;
     
-  }
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . 'functions.php';
+    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/functions.php';
 
-$password = '<your API password>';
-$handle = '<your Bluesky handle>';
-$text = 'Hello World! https://spokenlikeageek.com';
- 
-// connect to Bluesky API
-$connection = bluesky_connect($handle, $password);
+    $handle = '<your Bluesky handle>';
+    $password = '<your API password>';
+    $filename = '<local or remote path to image>';
+    $link = '<url of page for the link card>';
+    $text = 'Hello World! https://spokenlikeageek.com';
+    
+    // connect to Bluesky API
+    $connection = bluesky_connect($handle, $password);
 
-// example use
-//tbd
+    // send a text only post with link
+    $response = post_to_bluesky($connection, $text);
+    print_r($response);
+
+    // send an image with text and a link
+    $image = upload_media_to_bluesky($connection, $filename);
+    $response = post_to_bluesky($connection, $text, $image);
+    print_r($response);
+
+    // send text with link for a link card
+    // if you specifiy both media and link the latter takes precedence
+    $response = post_to_bluesky($connection, $text, '', $link);
+    print_r($response);
 
 ?>
